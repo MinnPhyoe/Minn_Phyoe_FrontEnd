c17dfca3364155a372fd1a7a9783a808f060927b
MinnPhyoe <minphyomon2919@gmail.com>
2025-11-25 23:37:55 +0000

    frontend

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Minn_Phyoe_Frontend/'
+    ? '/Minn_Phyoe_FrontEnd/'
     : '/',
-  devServer: {
-    port: 8080
-  }
-})
+});
